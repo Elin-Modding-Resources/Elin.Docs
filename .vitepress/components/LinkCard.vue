b79b064ae7b76c243e641e21145a0a5d32a06193
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-04-22 20:01:00 -0500

    update icon style

--- .vitepress/components/LinkCard.vue
+++ .vitepress/components/LinkCard.vue
@@ -3,23 +3,29 @@
     @click="goto"
     class="link-card group my-3 cursor-pointer rounded-lg bg-blue-200/30 p-4 shadow-sm shadow-blue-400/60 transition-all duration-300 hover:scale-[0.99] hover:bg-blue-100/80 dark:bg-sky-950 dark:shadow-sky-900/80 dark:hover:bg-sky-950/80"
   >
-    <div class="flex items-center">
-      <!-- Conditionally render the icon if it exists -->
-      <div v-if="i" class="shrink-0 mr-4">
+    <div class="flex items-stretch gap-4">
+      <div 
+        class="w-20 h-20 shrink-0 overflow-hidden rounded-md flex items-center justify-center"
+        v-if="i"
+      >
         <img
           :src="i"
-          class="h-14 w-14 rounded-full sm:h-17.5 sm:w-17.5 overflow-hidden"
+          class="w-full h-full object-contain block align-middle"
         />
       </div>
-      <div class="flex-1">
-        <header class="font-bold text-lg">{{ t }}</header>
+
+      <div class="flex flex-col justify-center flex-1 min-w-0">
+        <header class="font-bold text-lg leading-tight">
+          {{ t }}
+        </header>
+
+        <footer
+          class="mt-1 text-sm text-slate-500 transition-all duration-300 line-clamp-1 group-hover:line-clamp-none dark:text-slate-400 dark:group-hover:text-sky-400"
+        >
+          {{ u }}
+        </footer>
       </div>
     </div>
-    <footer
-      class="mt-1 transition-all duration-300 line-clamp-1 hover:line-clamp-none text-slate-500 dark:text-slate-400 dark:group-hover:text-sky-400"
-    >
-      {{ u }}
-    </footer>
   </div>
 </template>
 
